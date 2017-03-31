c9bc46389457edc47ba9fc95ff3cdded2e7fe780
Ignacio Cruz <igmoweb@gmail.com>
2017-03-31 17:07:29 +0200

    Un poco de tabulación

--- functions.php
+++ functions.php
@@ -210,16 +210,16 @@ function main_image_alma() {
   $files = get_children('post_parent='.get_the_ID().'&post_type=attachment
   &post_mime_type=image&order=desc');
   if($files) :
-  $keys = array_reverse(array_keys($files));
-  $j=0;
-  $num = $keys[$j];
-  $image=wp_get_attachment_image($num, 'portfolio-thumbnail', true);
-  $imagepieces = explode('"', $image);
-  $imagepath = $imagepieces[1];
-  $main=wp_get_attachment_url($num);
-  $template=get_template_directory();
-  $the_title=get_the_title();
-  print "$image";
+	  $keys = array_reverse(array_keys($files));
+	  $j=0;
+	  $num = $keys[$j];
+	  $image=wp_get_attachment_image($num, 'portfolio-thumbnail', true);
+	  $imagepieces = explode('"', $image);
+	  $imagepath = $imagepieces[1];
+	  $main=wp_get_attachment_url($num);
+	  $template=get_template_directory();
+	  $the_title=get_the_title();
+	  print "$image";
   endif;
 }
 
@@ -230,16 +230,16 @@ function main_image() {
   $files = get_children('post_parent='.get_the_ID().'&post_type=attachment
   &post_mime_type=image&order=desc');
   if($files) :
-  $keys = array_reverse(array_keys($files));
-  $j=0;
-  $num = $keys[$j];
-  $image=wp_get_attachment_image($num, 'featured', true);
-  $imagepieces = explode('"', $image);
-  $imagepath = $imagepieces[1];
-  $main=wp_get_attachment_url($num);
-  $template=get_template_directory();
-  $the_title=get_the_title();
-  print "$image";
+	  $keys = array_reverse(array_keys($files));
+	  $j=0;
+	  $num = $keys[$j];
+	  $image=wp_get_attachment_image($num, 'featured', true);
+	  $imagepieces = explode('"', $image);
+	  $imagepath = $imagepieces[1];
+	  $main=wp_get_attachment_url($num);
+	  $template=get_template_directory();
+	  $the_title=get_the_title();
+	  print "$image";
   endif;
 }
 
